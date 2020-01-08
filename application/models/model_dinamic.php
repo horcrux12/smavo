1c7bc16d0a15f7526a17c16e8a9b58b26dbcc36e
horcrux12 <silo.mardadi1@gmail.com>
2020-01-08 19:00:21 +0700

    model dinamis

---
+++ application/models/model_dinamic.php
@@ -0,0 +1,47 @@
+<?php 
+	/**
+	 * 
+	 */
+	class Model_dinamic extends CI_Model
+	{
+		
+		function getData ($table)
+		{
+			$this->db->select('*');
+			$this->db->from($table);
+			$query = $this->db->get();
+			return $query->result();
+		}
+
+		function getWhere ($table,$field,$where){
+			$this->db->where($field,$where);
+			$query = $this->db->get($table);
+			return $query;
+		}
+
+		function getDataWhere ($table,$field,$where)
+		{
+			$this->db->select('*');
+			$this->db->from($table);
+			$this->db->where($field,$where);
+			$query = $this->db->get($table);
+			return $query->result();
+		}	
+
+		function input_data($data,$table){
+        $this->db->insert($table, $data);
+    	}
+
+    	function update_data($field,$where,$data,$table){
+        $this->db->where($field,$where);
+        $this->db->update($table,$data);
+    	}
+
+    	public function delete_data($table,$field,$where) // menghapus data guru
+		{
+			$this->db->where($field,$where);
+			$this->db->delete($table);
+		}
+
+	}
+?>
