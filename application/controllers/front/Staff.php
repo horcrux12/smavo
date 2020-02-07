7ea3a40c46f6bbb4d4d9dbe3445a146fedab7097
horcrux12 <silo.mardadi1@gmail.com>
2020-02-07 13:30:09 +0700

    smavo

--- application/controllers/front/Staff.php
+++ application/controllers/front/Staff.php
@@ -8,27 +8,11 @@
         {
             $page_content['page'] ='front/guru/v_guru';
             $page_content['css'] ='
-            <link href="'.base_url().'assets2/css/style1.css" rel="stylesheet">
+            <link href="'.base_url().'assets2/css/style.css" rel="stylesheet">
 		    <link href="'.base_url().'assets2/styleslider.css" rel="stylesheet">';
             $page_content['js'] ='
 		    <script src="'.base_url().'assets2/js/main.js"></script>
-            <script src="'.base_url().'assets2/js/scripts.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-scale.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-meta.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-fetch.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-orientation.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-exif.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-exif-map.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-iptc.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/load-image-iptc-map.js"></script>
-            <!--
-            jQuery and Jcrop are not required by JavaScript Load Image,
-            but included for the demo
-            -->
-            <script src="'.base_url().'assets2/JSLoadImage/js/vendor/jquery.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/vendor/jquery.Jcrop.js"></script>
-            <script src="'.base_url().'assets2/JSLoadImage/js/demo/demo.js"></script>';
+            <script src="'.base_url().'assets2/js/scripts.js"></script>';
             $page_content['title'] ='';
 
             $jabatan = $this->model_dinamic->getData('tb_jabatan');
--- assets2/js/scripts.js
+++ assets2/js/scripts.js
@@ -156,10 +156,10 @@ $(function() {
 
 
     // countUp
-    $('.numbers .count').countUp({
-        delay: 10,
-        time: 1500
-    });
+    // $('.numbers .count').countUp({
+    //     delay: 10,
+    //     time: 1500
+    // });
 
 
     // Services Tabs
@@ -182,75 +182,6 @@ $(function() {
 
     });
 
-
-});
-
-
-// === window When Loading === //
-
-$(window).on("load",function (){
-
-    var wind = $(window);
-
-    // Preloader
-    $(".loading").fadeOut(500);
-
-
-    // stellar
-    wind.stellar();
-
-    // isotope
-    $('.gallery').isotope({
-      // options
-      itemSelector: '.items'
-    });
-
-    var $gallery = $('.gallery').isotope({
-      // options
-    });
-
-    // filter items on button click
-    $('.filtering').on( 'click', 'span', function() {
-
-        var filterValue = $(this).attr('data-filter');
-
-        $gallery.isotope({ filter: filterValue });
-
-    });
-
-    $('.filtering').on( 'click', 'span', function() {
-
-        $(this).addClass('active').siblings().removeClass('active');
-
-    });
-
-
-    // contact form validator
-    // $('#contact-form').validator();
-
-    // $('#contact-form').on('submit', function (e) {
-    //     if (!e.isDefaultPrevented()) {
-    //         var url = "contact.html";
-
-    //         $.ajax({
-    //             type: "POST",
-    //             url: url,
-    //             data: $(this).serialize(),
-    //             success: function (data)
-    //             {
-    //                 var messageAlert = 'alert-' + data.type;
-    //                 var messageText = data.message;
-
-    //                 var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-    //                 if (messageAlert && messageText) {
-    //                     $('#contact-form').find('.messages').html(alertBox);
-    //                     $('#contact-form')[0].reset();
-    //                 }
-    //             }
-    //         });
-    //         return false;
-    //     }
-    // });
 
 });
 
