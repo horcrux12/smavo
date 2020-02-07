886668035742e7a5bc15ee5e98624d96c9da0e17
alhafizhir <ralhafizhi@gmail.com>
2020-02-07 13:46:01 +0700

    rev

--- application/controllers/guru.php
+++ application/controllers/guru.php
@@ -71,131 +71,124 @@
 		}
 
 
-		public function tambah() // tambah data guru
-		{
-			$konten['css']			= '
-				<link rel="stylesheet" href="'.base_url().'assets/css/jquery-ui.custom.min.css" />
-				<link rel="stylesheet" href="'.base_url().'assets/css/chosen.min.css" />';
-			$konten['js']			= '
-			<script src="'.base_url().'assets/js/jquery-ui.custom.min.js"></script>
-			<script src="'.base_url().'assets/js/chosen.jquery.min.js"></script>
-			<script src="'.base_url().'assets/js/autosize.min.js"></script>
-			<script src="'.base_url().'assets/js/jquery.inputlimiter.min.js"></script>
-			<script src="'.base_url().'assets/js/jquery.maskedinput.min.js"></script>
-			
-			<script>
-				jQuery(function($) {
-				$("#id-input-file-1 , #id-input-file-2").ace_file_input({
-					no_file:"No File ...",
-					btn_choose:"Choose",
-					btn_change:"Change",
-					droppable:false,
-					onchange:null,
-					thumbnail:false //| true | large
-					//whitelist:"gif|png|jpg|jpeg"
-					//blacklist:"exe|php"
-					//onchange:""
-					//
-				});
-
-				$("#id-input-file-3").ace_file_input({
-					style: "well",
-					btn_choose: "Drop files here or click to choose",
-					btn_change: null,
-					no_icon: "ace-icon fa fa-cloud-upload",
-					droppable: true,
-					thumbnail: "small"//large | fit
-					//,icon_remove:null//set null, to hide remove/reset button
-					/**,before_change:function(files, dropped) {
-						//Check an example below
-						//or examples/file-upload.html
-						return true;
-					}*/
-					/**,before_remove : function() {
-						return true;
-					}*/
-					,
-					preview_error : function(filename, error_code) {
-						//name of the file that failed
-						//error_code values
-						//1 = "FILE_LOAD_FAILED",
-						//2 = "IMAGE_LOAD_FAILED",
-						//3 = "THUMBNAIL_FAILED"
-						//alert(error_code);
-					}
-			
-				}).on("change", function(){
-					//console.log($(this).data("ace_input_files"));
-					//console.log($(this).data("ace_input_method"));
-				});
-			});
-				</script>';
-			$konten['konten'] 		= 'guru/form_tambah_guru';
-			$konten['judul']		= 'Data Master';
-			$konten['sub_judul'] 	= 'Tambah Data guru';
-			$konten['kode'] = $this->model_guru->kode(); //tambah kode otomatis		
-			$this->load->view('v_dashboard',$konten);
-		}
-
-
-		public function simpan() // simpan data guru
-		{
-		  $config['upload_path'] 	= './assets/photo/guru/';
-	      $config['allowed_types'] 	= 'gif|jpg|jpeg|png';
-	      $config['encrypt_name']	= FALSE;
-		  $config['overwrite']		= true;
-	 
-	      $this->load->library('upload', $config);
-	      $this->upload->initialize($config);
-	 
-	      if ( ! $this->upload->do_upload('file_name')){
-	        $this->session->set_flashdata('info_gagal','Foto Gagal Diupload,Silakan pilih foto yang lain');
-	        //  redirect('guru/tambah'); 
-	      } else {
-			
-			$key = $this->input->post('kode');
-			$data['id_guru'] 					= $this->input->post('kode');
-			$data['nip'] 						= $this->input->post('nip');
-			$data['nama_lengkap'] 				= $this->input->post('nama');
-			$data['tempat_lahir'] 				= $this->input->post('tempat');
-			$data['tgl_lahir'] 					= $this->input->post('tanggal');
-			$data['id_jabatan'] 				= $this->input->post('jbt');
-			if (!$this->input->post('mapel')) {
-				$data['id_mapel'] 				= null;
-			}else {
-				$data['id_mapel'] 					= $this->input->post('mapel');
-
-			}
-			$data['foto'] 						= $this->upload->data('file_name'); 
-			// $datas = $this->upload->data();
-			// $datass = base_url().'assets/photo/guru/'.$data['foto'];
-			// print_r($datass);
-			// $image = create_img()
-			// echo "<pre>";
-			// imagerotate($datass,90,0);
-			// echo "</pre>";
-
-			$this->load->model('model_guru');
-			$query = $this->model_guru->getdata($key);
-			if($query->num_rows()>0)
-
+			public function tambah() // tambah data guru
 			{
+				$konten['css']			= '
+					<link rel="stylesheet" href="'.base_url().'assets/css/jquery-ui.custom.min.css" />
+					<link rel="stylesheet" href="'.base_url().'assets/css/chosen.min.css" />';
+				$konten['js']			= '
+				<script src="'.base_url().'assets/js/jquery-ui.custom.min.js"></script>
+				<script src="'.base_url().'assets/js/chosen.jquery.min.js"></script>
+				<script src="'.base_url().'assets/js/autosize.min.js"></script>
+				<script src="'.base_url().'assets/js/jquery.inputlimiter.min.js"></script>
+				<script src="'.base_url().'assets/js/jquery.maskedinput.min.js"></script>
+				
+				<script>
+					jQuery(function($) {
+					$("#id-input-file-1 , #id-input-file-2").ace_file_input({
+						no_file:"No File ...",
+						btn_choose:"Choose",
+						btn_change:"Change",
+						droppable:false,
+						onchange:null,
+						thumbnail:false //| true | large
+						//whitelist:"gif|png|jpg|jpeg"
+						//blacklist:"exe|php"
+						//onchange:""
+						//
+					});
 
-				$this->model_guru->getupdate($key,$data);
-				$this->session->set_flashdata('info','Data berhasil di update');
+					$("#id-input-file-3").ace_file_input({
+						style: "well",
+						btn_choose: "Drop files here or click to choose",
+						btn_change: null,
+						no_icon: "ace-icon fa fa-cloud-upload",
+						droppable: true,
+						thumbnail: "small"//large | fit
+						//,icon_remove:null//set null, to hide remove/reset button
+						/**,before_change:function(files, dropped) {
+							//Check an example below
+							//or examples/file-upload.html
+							return true;
+						}*/
+						/**,before_remove : function() {
+							return true;
+						}*/
+						,
+						preview_error : function(filename, error_code) {
+							//name of the file that failed
+							//error_code values
+							//1 = "FILE_LOAD_FAILED",
+							//2 = "IMAGE_LOAD_FAILED",
+							//3 = "THUMBNAIL_FAILED"
+							//alert(error_code);
+						}
+				
+					}).on("change", function(){
+						//console.log($(this).data("ace_input_files"));
+						//console.log($(this).data("ace_input_method"));
+					});
+				});
+					</script>';
+				$konten['konten'] 		= 'guru/form_tambah_guru';
+				$konten['judul']		= 'Data Master';
+				$konten['sub_judul'] 	= 'Tambah Data guru';
+				$konten['kode'] = $this->model_guru->kode(); //tambah kode otomatis		
+				$this->load->view('v_dashboard',$konten);
 			}
-			else
+
+
+			public function simpan() // simpan data guru
 			{
-				$this->model_guru->getinsert($data);
-				$this->session->set_flashdata('info','Data berhasil di simpan');
-			}
-			redirect('admin/guru');
+			$config['upload_path'] 	= './assets/photo/guru/';
+			$config['allowed_types'] 	= 'gif|jpg|jpeg|png';
+			$config['encrypt_name']	= FALSE;
+			$config['overwrite']		= true;
 		
+			$this->load->library('upload', $config);
+			$this->upload->initialize($config);
+		
+			if ( ! $this->upload->do_upload('file_name')){
+				$this->session->set_flashdata('info_gagal','Foto Gagal Diupload,Silakan pilih foto yang lain');
+				//  redirect('guru/tambah'); 
+			} else {
+				
+				$key = $this->input->post('kode');
+				$data['id_guru'] 					= $this->input->post('kode');
+				$data['nip'] 						= $this->input->post('nip');
+				$data['nama_lengkap'] 				= $this->input->post('nama');
+				$data['tempat_lahir'] 				= $this->input->post('tempat');
+				$data['tgl_lahir'] 					= $this->input->post('tanggal');
+				$data['id_jabatan'] 				= $this->input->post('jbt');
+				if (!$this->input->post('mapel')) {
+					$data['id_mapel'] 				= null;
+				}else {
+					$data['id_mapel'] 					= $this->input->post('mapel');
 
-		} 
+				}
+				$data['foto'] 						= $this->upload->data('file_name'); 
+
+				$this->load->model('model_guru');
+				$query = $this->model_guru->getdata($key);
+				if($query->num_rows()>0)
+
+				{
+
+					$this->model_guru->getupdate($key,$data);
+					$this->session->set_flashdata('info','Data berhasil di update');
+				}
+				else
+				{
+					$this->model_guru->getinsert($data);
+					$this->session->set_flashdata('info','Data berhasil di simpan');
+				}
+				redirect('admin/guru');
+			
+
+			} 
 
 
-	}
+		}
 
 		public function ubah($id) // Mengubah data guru
 		{
